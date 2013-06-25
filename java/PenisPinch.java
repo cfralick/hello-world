8d422c2975e8d79de3fd2ecc9d46dc4f3fe28f2a
Clint Fralick <cfralick@pandemiclabs.com>
2013-06-25 01:33:44 -0400

    Penispinching in Java

---
+++ java/PenisPinch.java
@@ -0,0 +1,20 @@
+/**
+ * public class PenisPinch
+ * pinches penis
+ */
+public class PenisPinch {
+    /**
+     * main: Public static function Penis PINCH! 
+     */
+    public static void main(String[] args) {
+        PenisPinch penis = new PenisPinch();
+        penis.pinch();
+    }
+    
+    /**
+     * pinches penis
+     */
+    public void pinch() {
+        System.out.println("Ouch!");
+    }
+}
